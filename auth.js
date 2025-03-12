18b9002a2aca82dd0b0262b8a80d417f6c08ac85
Your Name <you@example.com>
2025-03-12 13:31:17 -0700

    Initial commit

--- auth.js
+++ auth.js
@@ -5,10 +5,10 @@ const jwt = require('jsonwebtoken');
 const router = express.Router();
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'if0_38355218_educational'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER, 
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 // Registration endpoint
